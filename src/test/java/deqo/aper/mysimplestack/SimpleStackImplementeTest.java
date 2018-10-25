90434b9dbbfac359a96d6973346db408121c6925
Alexandre Perge <alexandre.perge@univ-tlse3.fr>
2018-10-25 09:23:15 +0200

    #1 ajout SimpleStackImplementeTest

---
+++ src/test/java/deqo/aper/mysimplestack/SimpleStackImplementeTest.java
@@ -0,0 +1,28 @@
+package deqo.aper.mysimplestack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleStackImplementeTest {
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void getSize() {
+    }
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void pop() {
+    }
+}
